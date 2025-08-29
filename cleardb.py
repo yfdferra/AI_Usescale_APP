6c99f75143141782f28cf3cda2b10bdd4736b967
DipRRai <superderped@gmail.com>
2025-08-29 18:26:20 +1000

    added temporary side bar and formatting

--- cleardb.py
+++
@@ -1,8 +0,0 @@
-ans = input('type "confirm" clear databases')
-if ans == 'confirm':
-    import os
-    
-    for i in os.listdir('./database'):
-        if i.endswith('.db'):
-            os.remove('./database/'+i)
-            print(f'removed {i}')
