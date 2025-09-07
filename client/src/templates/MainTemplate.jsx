90b297d5d28e5d0edfc127c76d381851d2e63eef
superderped <24651666+DipRRai@users.noreply.github.com>
2025-09-07 21:14:07 +1000

    minor bug fix

--- client/src/templates/MainTemplate.jsx
+++ client/src/templates/MainTemplate.jsx
@@ -4,6 +4,7 @@ import BaseTemplatesSection from "../components/BaseTemplatesSection";
 import CustomTemplatesSection from "../components/CustomTemplatesSection";
 import Dashboard from "../components/Dashboard";
 import { useState, useEffect } from "react";
+import HOST from "../GLOBALS/Globals.jsx";
 
 // [
 //             "INFO30006 written",
@@ -17,17 +18,16 @@ import { useState, useEffect } from "react";
 //           ]
 export default function MainTemplate({ children }) {
   const [templates, setTemplates] = useState([]);
-  var HOST = "http://localhost:5000";
   useEffect(() => {
     fetch(HOST + "/get_use_scales")
       .then((res) => res.json())
       .then((data) => {
-        setTemplates(data.map((item) => 
-          ({
+        setTemplates(
+          data.map((item) => ({
             id: item.usescale_id,
-            title: item.title
-          })
-        ));
+            title: item.title,
+          }))
+        );
       })
       .catch((error) => console.log("Error usescales not found"));
   }, []);
