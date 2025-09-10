8e961289104dd29d7aa314252ec9d0455678161e
federico.f <ferracin.f@student.unimelb.edu.au>
2025-09-10 13:38:19 +1000

    Merging Frontend (cleaned up) to Dev branch 2.0

--- client/src/templates/MainTemplate.jsx
+++ client/src/templates/MainTemplate.jsx
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 import React, { use } from "react";
-=======
-import React from "react";
->>>>>>> Frontend
 import Sidebar from "../components/Sidebar";
 import BaseTemplatesSection from "../components/BaseTemplatesSection";
 import CustomTemplatesSection from "../components/CustomTemplatesSection";
 import Dashboard from "../components/Dashboard";
-<<<<<<< HEAD
 import { useState, useEffect } from "react";
 import HOST from "../GLOBALS/Globals.jsx";
 
@@ -21,7 +16,7 @@ import HOST from "../GLOBALS/Globals.jsx";
 //             "More",
 //             "Overflow Row",
 //           ]
-export default function MainTemplate({ children }) {
+export default function MainTemplate({ children, onWrittenAssessmentClick }) {
   const [templates, setTemplates] = useState([]);
   useEffect(() => {
     fetch(HOST + "/get_use_scales")
@@ -37,34 +32,14 @@ export default function MainTemplate({ children }) {
       .catch((error) => console.log("Error usescales not found"));
   }, []);
 
-=======
-
-export default function MainTemplate({ children, onWrittenAssessmentClick }) {
->>>>>>> Frontend
   return (
     <div className="layout">
       <Sidebar />
       <Dashboard>
-<<<<<<< HEAD
-        <BaseTemplatesSection />
-        <CustomTemplatesSection templates={templates} />
-=======
         <BaseTemplatesSection
           onWrittenAssessmentClick={onWrittenAssessmentClick}
         />
-        <CustomTemplatesSection
-          templates={[
-            "INFO30006 written",
-            "INFO30006 presentation",
-            "Another",
-            "More",
-            "More",
-            "More",
-            "More",
-            "Overflow Row",
-          ]}
-        />
->>>>>>> Frontend
+        <CustomTemplatesSection templates={templates} />
       </Dashboard>
     </div>
   );
--- client/src/templates/UseScalePage.jsx
+++ client/src/templates/UseScalePage.jsx
@@ -1,49 +1,8 @@
-<<<<<<< HEAD
-import React from "react";
-=======
 import React, { useState } from "react";
->>>>>>> Frontend
 import Sidebar from "../components/Sidebar";
 import HorizontalSidebar from "../components/HorizontalSidebar";
 import VerticalDropdown from "../components/VerticalDropdown";
 import UseScaleBlock from "../components/UseScaleBlock";
-<<<<<<< HEAD
-
-export default function UseScalePage() {
-  return (
-    <div className="layout" style={{ position: "relative", height: "100vh" }}>
-      <Sidebar />
-      <HorizontalSidebar>
-        {open ? (
-          <div style={{ width: "100%" }}>
-            <VerticalDropdown title="Written Assessments">
-              {/* Any dropdown content here */}
-              <UseScaleBlock level="LEVEL N" label="NO AI" labelBg="#ffb3b3" />
-              <UseScaleBlock level="LEVEL R-1" label="" labelBg="#ffcfb3ff" />
-              <UseScaleBlock level="LEVEL R-2" label="" labelBg="#ffffb3ff" />
-              <UseScaleBlock level="LEVEL G" label="" labelBg="#d9b3ffff" />
-            </VerticalDropdown>
-
-            <VerticalDropdown title="Coding Assessments">
-              {/* Any dropdown content here */}
-              <div>Dropdown content goes here!</div>
-            </VerticalDropdown>
-
-            <VerticalDropdown title="Oral Assessments">
-              {/* Any dropdown content here */}
-              <div>Dropdown content goes here!</div>
-            </VerticalDropdown>
-
-            <VerticalDropdown title="Presentation Assessments">
-              {/* Any dropdown content here */}
-              <div>Dropdown content goes here!</div>
-            </VerticalDropdown>
-          </div>
-        ) : (
-          <span style={{ fontSize: 24 }}>≡</span>
-        )}
-      </HorizontalSidebar>
-=======
 import FilterSearchBar from "../components/FilterSearchBar";
 import TableSection from "../components/TableSection";
 import "./UseScalePage.css";
@@ -102,7 +61,6 @@ export default function UseScalePage() {
       <div className="use-scale-page-content">
         <TableSection />
       </div>
->>>>>>> Frontend
     </div>
   );
 }
