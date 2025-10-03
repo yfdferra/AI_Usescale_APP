3579684d3e44e6a38acaa6e299df4b14e3124d56
Annej2026 <53411273+Annej2026@users.noreply.github.com>
2025-10-03 17:53:39 +1000

    Comments for readability

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,12 @@ import "./index.css";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx"; // <-- add this
+import App from "./App.jsx";
 
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+/* renders app inside of root */
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter> 
