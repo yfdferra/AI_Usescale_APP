98e602e0eec9fbe951e272efc835fd8c6dc862e6
federico.f <ferracin.f@student.unimelb.edu.au>
2025-10-16 11:45:04 +1100

    changed Globals.jsx constant

--- client/src/GLOBALS/Globals.jsx
+++ client/src/GLOBALS/Globals.jsx
@@ -1,2 +1,7 @@
-const HOST = "http://localhost:5000";
+// const HOST = "http://localhost:5000";
+const HOST =
+  import.meta.env.PROD
+    ? ""                // in Heroku, use same domain
+    : "http://localhost:5000";  // in local dev, use Flask
+
 export default HOST;
