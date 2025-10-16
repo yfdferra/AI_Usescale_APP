d0606e9f99dd15a09e1676ea8924a197d67dece9
federico.f <ferracin.f@student.unimelb.edu.au>
2025-10-16 18:54:31 +1100

    Heroku CD Deploy Test 1

--- client/src/GLOBALS/Globals.jsx
+++ client/src/GLOBALS/Globals.jsx
@@ -1,2 +1,7 @@
-const HOST = "http://localhost:5000";
+const IS_PROD = process.env.NODE_ENV === "production";  // for heroku
+
+const HOST = IS_PROD ? "" : "http://localhost:5000";
+
+// const HOST = "http://localhost:5000";
+
 export default HOST;
