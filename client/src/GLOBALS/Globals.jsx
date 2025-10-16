3b3ef1f6d4dc04452dbba096edcb2cfccb6ed8c4
federico.f <ferracin.f@student.unimelb.edu.au>
2025-10-16 11:51:26 +1100

    rebuild frontend for deploy (global.jsx changed again)

--- client/src/GLOBALS/Globals.jsx
+++ client/src/GLOBALS/Globals.jsx
@@ -1,7 +1,6 @@
 // const HOST = "http://localhost:5000";
-const HOST =
-  import.meta.env.PROD
-    ? ""                // in Heroku, use same domain
-    : "http://localhost:5000";  // in local dev, use Flask
+const IS_PROD = process.env.NODE_ENV === "production";
+
+const HOST = IS_PROD ? "" : "http://localhost:5000";
 
 export default HOST;
